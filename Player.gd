87d32bcbf96d9f366d2949575380777c5cbb2f3b
BrunoM24 <brunocostamatos.24@gmail.com>
2019-04-27 21:18:09 +0100

    Made the required changes to export the game to PC and Android

--- Player.gd
+++ Player.gd
@@ -4,22 +4,30 @@ signal hit
 
 export var speed := 400 #How fast the player will move (pixels/sec)
 var screen_size #Size of the game window
+var velocity := Vector2()
+var target := Vector2() # Add this variable to hold the clicked position
 
 func _ready():
 	hide()
 	screen_size = get_viewport_rect().size
 	
 func _process(delta):
-	var velocity := Vector2() #The player's movement vector
+	# var velocity := Vector2() #The player's movement vector
 	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
+	if position.distance_to(target) > 10:
+		velocity = (target - position).normalized() * speed
+	else:
+		velocity = Vector2()
+	
+	# Keyboard controls removed
+	# if Input.is_action_pressed("ui_right"):
+	# 	velocity.x += 1
+	# if Input.is_action_pressed("ui_left"):
+	# 	velocity.x -= 1
+	# if Input.is_action_pressed("ui_up"):
+	# 	velocity.y -= 1
+	# if Input.is_action_pressed("ui_down"):
+	# 	velocity.y += 1
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -28,8 +36,11 @@ func _process(delta):
 		$AnimatedSprite.stop()
 		
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	
+	# Don't need to camp the player's position
+	# because we can't click outside of the screen
+	# position.x = clamp(position.x, 0, screen_size.x)
+	# position.y = clamp(position.y, 0, screen_size.y)
 	
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right"
@@ -46,5 +57,11 @@ func _on_Player_body_entered(body):
 	
 func start(pos : Vector2) -> void:
 	position = pos
+	target = pos
 	show()
 	$CollisionShape2D.disabled = false
+
+# Change the target whenever a touch event happens
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
